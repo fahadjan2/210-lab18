dcac62da485887447542cf5b6e91819c383acc52
fahadjan2 <fahadfawad2006@gmail.com>
2025-10-03 16:50:26 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void nodeAddHead(Node *&);
-void nodeAddTail(Node *&);
-void output(Node *);
-
 struct Node {
     double rating;
     string comment;
@@ -12,8 +8,28 @@ struct Node {
     Node * next;
 };
 
-int main () {
+void nodeAddHead(Node *&);
+void nodeAddTail(Node *&);
+void output(Node *);
 
+int main() {
+    int entry;
+    cout << "test1" << endl;
+    cout << "Which linked list method would you like to use?" << endl;
+    cout << "[1] New nodes at the head of the linked list\n[2] New nodes at the tail of the linked list" << endl;
+    cout << "Choice: ";
+    cin >> entry;
+    cout << endl;
+
+    while (entry != 1 && entry != 2) {
+        cout << "Enter a valid choice [1] or [2]" << endl;
+        cout << "Choice: ";
+        cin >> entry;
+        cout << endl;
+    }
+
+    Node * head = nullptr;
+    cout << "Enter Review Ratings" << endl;
 }
 
 void nodeAddHead(Node *&head) {
