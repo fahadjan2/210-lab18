67bcd5d318853abd430be4356b59876722049dfa
fahadjan2 <fahadfawad2006@gmail.com>
2025-10-03 16:41:16 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
 
+void nodeAddHead(Node *&);
+void nodeAddTail(Node *&);
+void output(Node *);
+
+struct Node {
+    double rating;
+    string comment;
+
+    Node * next;
+};
+
 int main () {
 
+}
+
+void nodeAddHead(Node *&head) {
+
+}
+
+void nodeAddTail(Node *&head) {
+
+}
+
+void output(Node * head) {
+    if (!head) {
+        cout << "Empty list" << endl;
+        return;
+    }
+
+    Node * current = head;
+    int count = 1;
+    while (current) {
+        cout << "Review #" << count++ << ": " << current->rating << ": " << current->comment << endl;
+        current = current->next;
+    }
 }
